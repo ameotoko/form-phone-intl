ed4cb5ebb10a8261c07e0b1e331bb0f488cf2775
ameotoko <andrey.vinichenko@gmail.com>
2023-12-26 01:40:57 +0100

    update assets path in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 
 gulp.task('default', function (cb) {
     gulp.src('node_modules/intl-tel-input/build/**')
-        .pipe(gulp.dest('src/Resources/public/'))
+        .pipe(gulp.dest('public/'))
     ;
 
     cb();
