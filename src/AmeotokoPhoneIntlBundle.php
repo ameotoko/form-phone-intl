f0f515bca9fecc1f728670b87d16dbcbdfa9fdde
ameotoko <andrey.vinichenko@gmail.com>
2023-12-26 01:11:50 +0100

    update bundle folders structure to the new standard

--- src/AmeotokoPhoneIntlBundle.php
+++ src/AmeotokoPhoneIntlBundle.php
@@ -10,4 +10,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AmeotokoPhoneIntlBundle extends Bundle
 {
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
 }
--- src/DependencyInjection/AmeotokoPhoneIntlExtension.php
+++ src/DependencyInjection/AmeotokoPhoneIntlExtension.php
@@ -15,7 +15,7 @@ class AmeotokoPhoneIntlExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yml');
     }
 }
